50e3301a76e8bf7a5b4413f50c62a4daa0961dd3
Nicholas-UFC <nicholas.fm4@gmail.com>
2026-08-25 10:03:03 -0300

    :tada: Primeiro commit.

---
+++ db/db.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.competition_download('rossmann-store-sales')
+
+print("Path to competition files:", path)
+# C:\Users\Admin\.cache\kagglehub\competitions\rossmann-store-sales
